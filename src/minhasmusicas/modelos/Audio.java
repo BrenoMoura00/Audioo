e61532eabe258d18dd84cc7011d0d8d71d100d3d
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-30 22:01:26 -0300

    Criando as classe Musica e PodCast com alguns atributos indivíduais para o diferencial da Super classe

--- src/minhasmusicas/modelos/Audio.java
+++ src/minhasmusicas/modelos/Audio.java
@@ -6,4 +6,31 @@ public class Audio {
     private int totalCurtidas;
     private double classificacao;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public double getClassificacao() {
+        return classificacao;
+    }
+
+    public void curte(){
+        this.totalCurtidas++;
+    }
+
+    public void reproduz(){
+        this.totalReproducoes++;
+    }
 }
