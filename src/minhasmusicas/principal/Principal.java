9f946b9af82598bafaa16df6e586396365c4938b
BrenoMoura00 <brenoluisadv@gmail.com>
2024-01-30 22:21:22 -0300

    Gerando reproduções e curtidas para o podcast e a musica junto com valores a cada atributo na classe principal

--- src/minhasmusicas/principal/Principal.java
+++ src/minhasmusicas/principal/Principal.java
@@ -1,4 +1,35 @@
 package minhasmusicas.principal;
 
+import minhasmusicas.modelos.Musica;
+import minhasmusicas.modelos.PodCast;
+
 public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Flowers");
+        minhaMusica.setCantor("Miley Cyrus");
+        minhaMusica.setGenero("Pop");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+        for (int i = 0; i < 500; i++) {
+            minhaMusica.curte();
+        }
+
+        PodCast meuPodCast = new PodCast();
+        meuPodCast.setTitulo("DevCast");
+        meuPodCast.setApresentador("Breno Moura");
+        meuPodCast.setDescricao("Episódio 01");
+
+        for (int i = 0; i < 5640; i++) {
+            meuPodCast.reproduz();
+        }
+
+        for (int i = 0; i < 2453; i++) {
+            meuPodCast.curte();
+        }
+
+
+    }
 }
